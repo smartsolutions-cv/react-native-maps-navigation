25a5c4d1e1ecf4e59dba887defb51ee35bc3cd70
Karan Mamtora <kmamtora@sandatasystem.com>
2021-04-20 02:00:36 +0530

    update

--- src/components/MapViewNavigation/index.js
+++ src/components/MapViewNavigation/index.js
@@ -219,12 +219,13 @@ export default class MapViewNavigation extends Component {
    * @param heading
    * @param duration
    */
-  updatePosition(coordinate, heading, duration = 0) {
+  updatePosition(coordinate, heading = 0, duration = 0) {
     this.props.map().animateCamera(
       {
         center: coordinate,
         heading: heading,
         zoom: this.props.navigationZoomLevel,
+        pitch: 90,
       },
       duration
     );
