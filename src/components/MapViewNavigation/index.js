871e11817c7bd6c5bede656aabbdc244b4656b44
Karan Mamtora <kmamtora@sandatasystem.com>
2021-04-20 01:35:16 +0530

    Fixed: animateToCoordinate() and animateToBearing() methods deprecated

--- src/components/MapViewNavigation/index.js
+++ src/components/MapViewNavigation/index.js
@@ -216,10 +216,18 @@ export default class MapViewNavigation extends Component {
   /**
    * updatePosition
    * @param coordinate
+   * @param heading
    * @param duration
    */
-  updatePosition(coordinate, duration = 0) {
-    this.props.map().animateToCoordinate(coordinate, duration);
+  updatePosition(coordinate, heading, duration = 0) {
+    this.props.map().animateCamera(
+      {
+        center: coordinate,
+        heading: heading,
+        zoom: this.props.navigationZoomLevel,
+      },
+      duration
+    );
   }
 
   /**
@@ -228,9 +236,11 @@ export default class MapViewNavigation extends Component {
    * @param duration
    */
   updateBearing(bearing, duration = false) {
-    this.props
-      .map()
-      .animateToBearing(bearing, duration || this.props.animationDuration);
+    const region = {
+      latitude: this.state.position.latitude,
+      longitude: this.state.position.longitude,
+    };
+    this.updatePosition(location, bearing, duration || 0);
   }
 
   /**
@@ -287,9 +297,7 @@ export default class MapViewNavigation extends Component {
 
     // update position on map
     if (this.state.navigationMode == NavigationModes.NAVIGATION) {
-      this.updatePosition(position);
-
-      this.updateBearing(heading);
+      this.updatePosition(position, heading);
     }
 
     this.setState({ position });
@@ -411,18 +419,15 @@ export default class MapViewNavigation extends Component {
     return this.prepareRoute(origin, destination, options, true).then(
       (route) => {
         const region = {
-          ...route.origin.coordinate,
-          ...this.getZoomValue(this.props.navigationZoomLevel),
+          // ...route.origin.coordinate,
+          // ...this.getZoomValue(this.props.navigationZoomLevel),
+          latitude: parseFloat(route.origin.coordinate.latitude),
+          longitude: parseFloat(route.origin.coordinate.latitude),
         };
 
-        this.props.map().animateToRegion(region, this.props.animationDuration);
-        this.props
-          .map()
-          .animateToViewingAngle(
-            this.props.navigationViewingAngle,
-            this.props.animationDuration
-          );
-
+        // this.props.map().animateToRegion(region, this.props.animationDuration);
+        // this.props.map().animateToViewingAngle(this.props.navigationViewingAngle,this.props.animationDuration);
+        this.props.animateCamera({ center: region, pitch: this.props.navigationViewingAngle, zoom: this.props.navigationZoomLevel }, this.props.animationDuration);            
         //this.updatePosition(route.origin.coordinate);
         this.updateBearing(route.initialBearing);
 
